fb3b6ffbc9c2ef10f594e62bf11e5de088e336d0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-12-09 05:13:33 +0530

    Basic query with Apollo-Client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import './App.css';
+
+const client = new ApolloClient({
+  // The only Argument that is 100% recommended by Apollo Client ~ GraphQL endpoint
+  uri: 'http://localhost:4000/'
+});
+
+client
+  .query({
+    query: gql`
+      {
+        recipes {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello World</div>
+      </ApolloProvider>
+    );
   }
 }
 
