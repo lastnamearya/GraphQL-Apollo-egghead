b05e2dc2617af03e84085f37a8b81451aa644a47
lastnamearya <arya.jigyasu6815@gmail.com>
2018-12-12 04:38:42 +0530

    Done with isStarred Mutation locally

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import recipesQuery from './recipesQuery';
+
+const updateRecipeStarredMutation = gql`
+  mutation updateRecipeStarred($id: ID!, $isStarred: Boolean) {
+    updateRecipeStarred(id: $id, isStarred: $isStarred) @client
+  }
+`;
 
 class Recipes extends Component {
   state = {
@@ -37,9 +44,42 @@ class Recipes extends Component {
                 {data.recipes.map(({ id, title, isStarred }) => (
                   <li key={id}>
                     {title}
-                    <span style={{ color: isStarred ? 'orange' : 'grey' }}>
-                      ★
-                    </span>
+                    <Mutation
+                      mutation={updateRecipeStarredMutation}
+                      refetchQueries={[
+                        {
+                          query: recipesQuery,
+                          variables: { vegetarian: true }
+                        },
+                        {
+                          query: recipesQuery,
+                          variables: { vegetarian: false }
+                        }
+                      ]}
+                      awaitRefetchQueries={true}
+                    >
+                      {(updateRecipeStarred, { loading, error }) => (
+                        <button
+                          onClick={() => {
+                            updateRecipeStarred({
+                              variables: {
+                                id,
+                                isStarred: !isStarred
+                              }
+                            });
+                          }}
+                          className="star-btn"
+                          style={{
+                            color: isStarred ? 'orange' : 'grey',
+                            animation: loading
+                              ? 'inflate 0.7s ease infinite alternate'
+                              : 'none'
+                          }}
+                        >
+                          ★ {error && 'Failed to update'}
+                        </button>
+                      )}
+                    </Mutation>
                   </li>
                 ))}
               </ul>
