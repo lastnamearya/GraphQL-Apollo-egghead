59768f72a29788a85d207b640475dc740e31742e
lastnamearya <arya.jigyasu6815@gmail.com>
2018-12-11 05:32:36 +0530

    Mutation Component with Apollo Queries

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import recipesQuery from './recipesQuery';
+
+const addRecipeMutation = gql`
+  mutation addRecipe($recipe: RecipeInput!) {
+    addRecipe(recipe: $recipe) {
+      id
+      title
+    }
+  }
+`;
 
 export default class AddRecipe extends Component {
   state = {
@@ -24,33 +36,60 @@ export default class AddRecipe extends Component {
 
   render() {
     return (
-      <form
-        onSubmit={evt => {
-          evt.preventDefault();
-          this.resetFields();
-        }}
+      <Mutation
+        mutation={addRecipeMutation}
+        refetchQueries={[
+          {
+            query: recipesQuery,
+            variables: { vegetarian: true }
+          },
+          {
+            query: recipesQuery,
+            variables: { vegetarian: false }
+          }
+        ]}
+        awaitRefetchQueries={true}
       >
-        <label>
-          <span>Title</span>
-          <input
-            type="text"
-            value={this.state.title}
-            onChange={this.updateTitle}
-          />
-        </label>
+        {(addRecipe, { loading, error }) => (
+          <form
+            onSubmit={evt => {
+              evt.preventDefault();
+              addRecipe({
+                variables: {
+                  recipe: {
+                    title: this.state.title,
+                    vegetarian: this.state.vegetarian
+                  }
+                }
+              });
+              this.resetFields();
+            }}
+          >
+            <label>
+              <span>Title</span>
+              <input
+                type="text"
+                value={this.state.title}
+                onChange={this.updateTitle}
+              />
+            </label>
 
-        <label>
-          <input
-            type="checkbox"
-            checked={this.state.vegetarian}
-            onChange={this.updateVegetarian}
-          />
-          <span>Vegetarian</span>
-        </label>
-        <div>
-          <button>Add Recipe</button>
-        </div>
-      </form>
+            <label>
+              <input
+                type="checkbox"
+                checked={this.state.vegetarian}
+                onChange={this.updateVegetarian}
+              />
+              <span>Vegetarian</span>
+            </label>
+            <div>
+              <button>Add Recipe</button>
+            </div>
+            {loading && <p>Adding a recipe -- please wait...</p>}
+            {error && <p>Error :( Please try again</p>}
+          </form>
+        )}
+      </Mutation>
     );
   }
 }
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import recipesQuery from './recipesQuery';
 
